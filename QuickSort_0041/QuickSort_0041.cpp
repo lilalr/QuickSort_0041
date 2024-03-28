4e2b72dad62b03b24c036855cc30cc31fe1653c2
lilalr <gurennut@gmail.com>
2024-03-28 17:29:54 +0700

    step 8

--- QuickSort_0041/QuickSort_0041.cpp
+++ QuickSort_0041/QuickSort_0041.cpp
@@ -65,4 +65,8 @@ void q_short(int low, int high)
         cmp_count++;
         //search for an element less than or equal to pivot
         while ((arr[j] > pivot) && (j >= low)) //step 7 
+        {
+            j--; //step 8
+            cmp_count++;
+        }
         
